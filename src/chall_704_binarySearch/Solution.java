3b9001c25f7d0bf0f0292cb6e5420c44e346dab1
João Marcos <101360915+joaomarcosmr@users.noreply.github.com>
2025-09-17 13:35:44 -0300

    more leetcode problms

---
+++ src/chall_704_binarySearch/Solution.java
@@ -0,0 +1,23 @@
+package chall_704_binarySearch;
+
+public class Solution {
+
+    // Complexity o(logN) because of the splitting, and the nums is already sorted
+    public int search(int[] nums, int target) {
+        int left = 0, right = nums.length - 1;
+
+        while(left <= right){
+            int mid = (int) Math.ceil((right + left) / 2);
+
+            if(nums[mid] == target){
+                return mid;
+            } else if (nums[mid] < target){
+                left = mid + 1;
+            } else {
+                right = mid - 1;
+            }
+        }
+
+        return -1;
+    }
+}
--- src/chall_88_mergeSortedArray/Solution.java
+++ src/chall_88_mergeSortedArray/Solution.java
@@ -1,22 +1,26 @@
-import java.util.Stack;
+package chall_88_mergeSortedArray;
+
+import java.util.ArrayList;
+import java.util.Collections;
+import java.util.List;
 
 public class Solution {
     // Since i'm using sorting the complexity is O(n*logN)
     public void merge(int[] nums1, int m, int[] nums2, int n) {
-        List<Integer> intergerList = new ArrayList<>();
+        List<Integer> integerList = new ArrayList<>();
 
         for(int i = 0; i < m; i++){
-            intergerList.add(nums1[i]);
+            integerList.add(nums1[i]);
         }
 
         for(int j : nums2){
-            intergerList.add(j);
+            integerList.add(j);
         }
 
         Collections.sort(integerList);
 
         for(int i = 0; i < nums1.length;i++){
-            nums[i] = intergerList.get(i);
+            nums1[i] = integerList.get(i);
         }
     }
 }
